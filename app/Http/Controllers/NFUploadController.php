61cd3d4ced7f1972385cd180828bd95f080afc7a
joão marcos <joaofrigo1@gmail.com>
2025-11-21 16:08:50 -0300

    DTD para NFE funcional

--- app/Http/Controllers/NFUploadController.php
+++ app/Http/Controllers/NFUploadController.php
@@ -10,30 +10,91 @@ class NFUploadController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'xmls.*' => 'required|file|mimes:xml|max:2048' // valida cada arquivo
+            'xmls.*' => 'required|file|mimes:xml|max:2048'
         ]);
 
-        $arquivos = $request->file('xmls'); // array de arquivos
+        $arquivos = $request->file('xmls');
         $mensagens = [];
 
         foreach ($arquivos as $file) {
             $filename = time() . '_' . $file->getClientOriginalName();
-            $conteudo = $file->get(); // lê conteúdo
+            $conteudo = $file->get(); // lê conteúdo XML
 
-            // grava no banco
+            libxml_use_internal_errors(true);
+
+            $dom = new \DOMDocument();
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = false;
+            $dom->loadXML($conteudo);
+
+            if (strpos($conteudo, '<nfeProc') !== false) {
+                $dtdPath = str_replace('\\', '/', base_path('validadores/nfe.dtd'));
+            } elseif (strpos($conteudo, '<ns3:RetornoConsulta') !== false) {
+                $dtdPath = str_replace('\\', '/', base_path('validadores/nfse.dtd'));
+            } else {
+                $mensagens[] = "Tipo de XML não reconhecido: $filename";
+                continue;
+            }
+
+            if (!file_exists($dtdPath)) {
+                $mensagens[] = "DTD não encontrado para: $filename";
+                continue;
+            }
+
+            $xmlWithDoctype = preg_replace(
+                '/<\?xml.*\?>/',
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE {$dom->documentElement->tagName} SYSTEM \"{$dtdPath}\">",
+                $conteudo
+            );
+
+            $dom = new \DOMDocument();
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = false;
+            $dom->loadXML($xmlWithDoctype);
+
+            if (!$dom->validate()) {
+                $errors = libxml_get_errors();
+                $mensagemErro = "Falha na validação DTD: $filename\n";
+                foreach ($errors as $error) {
+                    $mensagemErro .= $this->formatLibxmlError($error, "    "); // recuo de 4 espaços
+                }
+                libxml_clear_errors();
+                $mensagens[] = $mensagemErro;
+                continue;
+            }
+
             $registro = NotaFiscal::create([
                 'nome_arquivo' => $filename,
                 'xml_conteudo' => $conteudo,
             ]);
 
             $mensagens[] = "Nota salva no banco: $filename (ID: {$registro->id})";
-
-            // pequeno delay para nomes únicos
             usleep(1000);
         }
 
-        // redireciona com todas as mensagens
         return redirect('/')
-            ->with('msg', implode('<br>', $mensagens));
+            ->with('msg', implode("\n", $mensagens));
+    }
+
+    private function formatLibxmlError($error, $indent = "  ")
+    {
+        $return = $indent;
+        switch ($error->level) {
+            case LIBXML_ERR_WARNING:
+                $return .= "Warning {$error->code}: ";
+                break;
+            case LIBXML_ERR_ERROR:
+                $return .= "Error {$error->code}: ";
+                break;
+            case LIBXML_ERR_FATAL:
+                $return .= "Fatal Error {$error->code}: ";
+                break;
+        }
+        $return .= trim($error->message);
+        if ($error->file) {
+            $return .= " in {$error->file}";
+        }
+        $return .= " on line {$error->line}, column {$error->column}\n";
+        return $return;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,11 @@
 <h2>Enviar Notas Fiscais (XML)</h2>
 
 @if(session('msg'))
-    <p>{{ session('msg') }}</p>
+    <pre>{{ session('msg') }}</pre>
 @endif
 
 <form action="{{ route('upload.xml') }}" method="POST" enctype="multipart/form-data">
     @csrf
-    <!-- name com [] e atributo multiple para vários arquivos -->
     <input type="file" name="xmls[]" accept=".xml" multiple required>
     <button type="submit">Enviar</button>
 </form>
